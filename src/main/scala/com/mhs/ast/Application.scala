741f4ea42264aa70b2f84dafe6773d8de28bcae0
LSinzker <luisasf1408@gmail.com>
2017-07-04 21:41:01 -0300

    Remove comment

--- src/main/scala/com/mhs/ast/Application.scala
+++ src/main/scala/com/mhs/ast/Application.scala
@@ -2,8 +2,6 @@ package com.mhs.ast
 
 import com.mhs.memory.{ExpressionEnv, FuncEnv}
 
-/**
-  */
 class Application(name: String, args: Expression*) extends Expression {
   override def evaluate(): Value = {
     val function = FuncEnv.search(name)
